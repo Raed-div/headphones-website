b53878dbb6d84bc0a7319090e13691828415925d
Raed-div <Raed.div@gmail.com>
2025-12-26 02:09:41 +0300

    feat: add a new Footer component with social links, product links, and branding.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
-  const currentYear = new Date().getFullYear();
-
   const socialLinks = [
     {
       name: "Discord",
